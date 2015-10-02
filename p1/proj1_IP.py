eca641d2c9bf81fecb74ad6b30c08afb0885964b
Michael Ip <ip.w.michael@gmail.com>
2015-10-02 15:52:48 -0400

    shortest point to line segment function

--- p1/proj1_IP.py
+++ p1/proj1_IP.py
@@ -1,11 +1,11 @@
 '''
 Michael Ip
 
-	I pledge on my honor that I have not given or received
+    I pledge on my honor that I have not given or received
 any unauthorized assistance on this project.
-	Michael Ip, ip.w.michael@gmail.com
+    Michael Ip, ip.w.michael@gmail.com
 
-	** How The Code Works **
+    ** How The Code Works **
 
 '''
 
@@ -13,90 +13,90 @@ import math
 
 #Dana Nau's intersect function
 def intersect(e1,e2):
-	"""Return True if edges e1 and e2 intersect, False otherwise."""	   
-	
-	# First, grab all the coordinates
-	((x1a,y1a), (x1b,y1b)) = e1
-	((x2a,y2a), (x2b,y2b)) = e2
-	dx1 = x1a-x1b
-	dy1 = y1a-y1b
-	dx2 = x2a-x2b
-	dy2 = y2a-y2b
-	
-	if (dx1 == 0) and (dx2 == 0):		# both lines vertical
-		if x1a != x2a: return False
-		else: return collinear_point_in_edge((x1a,y1a),e2) or collinear_point_in_edge((x1b,y1b),e2)
-	if (dx2 == 0):		# e2 is vertical, so m2 = infty
-		x = x2a
-		# compute y = m1 * x + b1, but minimize roundoff error
-		y = (x2a-x1a)*dy1/float(dx1) + y1a
-		return collinear_point_in_edge((x,y),e1) and collinear_point_in_edge((x,y),e2) 
-	elif (dx1 == 0):		# e1 is vertical, so m1 = infty
-		x = x1a
-		# compute y = m2 * x + b2, but minimize roundoff error
-		y = (x1a-x2a)*dy2/float(dx2) + y2a
-		return collinear_point_in_edge((x,y),e1) and collinear_point_in_edge((x,y),e2) 
-	else:		# neither line is vertical
-		# check m1 = m2, without roundoff error:
-		if dy1*dx2 == dx1*dy2:		# same slope, so either parallel or collinear
-			# check b1 != b2, without roundoff error:
-			if dx2*dx1*(y2a-y1a) != dy2*dx1*x2a - dy1*dx2*x1a:
-				return False
-			return collinear_point_in_edge((x1a,y1a),e2) or collinear_point_in_edge((x1b,y1b),e2)
-		# compute x = (b2-b1)/(m1-m2) but minimize roundoff error:
-		x = (dx2*dx1*(y2a-y1a) - dy2*dx1*x2a + dy1*dx2*x1a)/float(dx2*dy1 - dy2*dx1)
-		# compute y = m1*x + b1 but minimize roundoff error
-		y = (dy2*dy1*(x2a-x1a) - dx2*dy1*y2a + dx1*dy2*y1a)/float(dy2*dx1 - dx2*dy1)
-	return collinear_point_in_edge((x,y),e1) and collinear_point_in_edge((x,y),e2) 
+    """Return True if edges e1 and e2 intersect, False otherwise."""       
+    
+    # First, grab all the coordinates
+    ((x1a,y1a), (x1b,y1b)) = e1
+    ((x2a,y2a), (x2b,y2b)) = e2
+    dx1 = x1a-x1b
+    dy1 = y1a-y1b
+    dx2 = x2a-x2b
+    dy2 = y2a-y2b
+    
+    if (dx1 == 0) and (dx2 == 0):        # both lines vertical
+        if x1a != x2a: return False
+        else: return collinear_point_in_edge((x1a,y1a),e2) or collinear_point_in_edge((x1b,y1b),e2)
+    if (dx2 == 0):        # e2 is vertical, so m2 = infty
+        x = x2a
+        # compute y = m1 * x + b1, but minimize roundoff error
+        y = (x2a-x1a)*dy1/float(dx1) + y1a
+        return collinear_point_in_edge((x,y),e1) and collinear_point_in_edge((x,y),e2) 
+    elif (dx1 == 0):        # e1 is vertical, so m1 = infty
+        x = x1a
+        # compute y = m2 * x + b2, but minimize roundoff error
+        y = (x1a-x2a)*dy2/float(dx2) + y2a
+        return collinear_point_in_edge((x,y),e1) and collinear_point_in_edge((x,y),e2) 
+    else:        # neither line is vertical
+        # check m1 = m2, without roundoff error:
+        if dy1*dx2 == dx1*dy2:        # same slope, so either parallel or collinear
+            # check b1 != b2, without roundoff error:
+            if dx2*dx1*(y2a-y1a) != dy2*dx1*x2a - dy1*dx2*x1a:
+                return False
+            return collinear_point_in_edge((x1a,y1a),e2) or collinear_point_in_edge((x1b,y1b),e2)
+        # compute x = (b2-b1)/(m1-m2) but minimize roundoff error:
+        x = (dx2*dx1*(y2a-y1a) - dy2*dx1*x2a + dy1*dx2*x1a)/float(dx2*dy1 - dy2*dx1)
+        # compute y = m1*x + b1 but minimize roundoff error
+        y = (dy2*dy1*(x2a-x1a) - dx2*dy1*y2a + dx1*dy2*y1a)/float(dy2*dx1 - dx2*dy1)
+    return collinear_point_in_edge((x,y),e1) and collinear_point_in_edge((x,y),e2) 
 
 
 def collinear_point_in_edge((x,y),((xa,ya),(xb,yb))):
-	"""
-	Helper function for intersect.
-	If (x,y) is collinear with the edge from (xa,ya) to (xb,yb), then
-	(x,y) is in the edge if
-		x is between xa and xb, inclusive, and
-		y is between ya and yb, inclusive.
-	The test of y is redundant except when line is vertical.
-	"""
-	if ((xa <= x <= xb) or (xb <= x <= xa)) \
-	   and ((ya <= y <= yb) or (yb <= y <= ya)):
-	   return True
-	return False
+    """
+    Helper function for intersect.
+    If (x,y) is collinear with the edge from (xa,ya) to (xb,yb), then
+    (x,y) is in the edge if
+        x is between xa and xb, inclusive, and
+        y is between ya and yb, inclusive.
+    The test of y is redundant except when line is vertical.
+    """
+    if ((xa <= x <= xb) or (xb <= x <= xa)) \
+       and ((ya <= y <= yb) or (yb <= y <= ya)):
+       return True
+    return False
 
 
 def crash(e, w):
-	'''
-	Return True if edge e intersects with any wall in w
-	Return False otherwise
-	'''
-	for wall in w:
-		if intersect(e, wall):
-			return True
-	return False
+    '''
+    Return True if edge e intersects with any wall in w
+    Return False otherwise
+    '''
+    for wall in w:
+        if intersect(e, wall):
+            return True
+    return False
 
 def velocities(s, w):
-	'''
-	Returns set of possible choices of velocities.
-	'''
-	toReturn = [] #list of possible choices
-	((x, y), (vx, vy)) = s
-	for i in range(-1,2):
-		for j in range(-1,2):
-			(vxTemp, vyTemp) = (vx + i, vy + j)
-			edge = ((x,y), (x+vxTemp, y+vyTemp))
-			if(not crash(edge, w)):
-				toReturn.append((vxTemp,vyTemp))
-	return toReturn
+    '''
+    Returns set of possible choices of velocities.
+    '''
+    toReturn = [] #list of possible choices
+    ((x, y), (vx, vy)) = s
+    for i in range(-1,2):
+        for j in range(-1,2):
+            (vxTemp, vyTemp) = (vx + i, vy + j)
+            edge = ((x,y), (x+vxTemp, y+vyTemp))
+            if(not crash(edge, w)):
+                toReturn.append((vxTemp,vyTemp))
+    return toReturn
 
 def find_path(pnot, finish, w, h):
-	'''
-	pnot is the starting point
-	finish is the finish line (an edge)
-	w is the list of walls (edges)
-	h is heuristic function h(s, finish, w) where s is any state s = (p, v)
-	'''
-	(x,y) = pnot
+    '''
+    pnot is the starting point
+    finish is the finish line (an edge)
+    w is the list of walls (edges)
+    h is heuristic function h(s, finish, w) where s is any state s = (p, v)
+    '''
+    (x,y) = pnot
 
 def heur(s, finish, w):
     """
@@ -105,15 +105,40 @@ def heur(s, finish, w):
     ((x, y), (vx, vy)) = s
     ((x1a, y1a), (x2b, y2b)) = finish
     nextPoint = (x+vx, y + vy)
-    straightLine = dist((x,y),
+    straightLine = dist((x,y))
+
+def distPtLine(p0, line):
+
+    ((x1, y1),(x2,y2)) = line
+    (x3,y3) = p0
+    px = x2-x1
+    py = y2-y1
+    temp = px**2 + py**2
+
+    u =  ((x3 - x1) * px + (y3 - y1) * py) / float(temp)
+    if u > 1:
+        u = 1
+    elif u < 0:
+        u = 0
+
+    x = x1 + u * px
+    y = y1 + u * py
+
+    dx = x - x3
+    dy = y - y3
+
+    dist = math.sqrt(dx*dx + dy*dy)
+
+    return dist
+
 
 def dist(p0,p1):
-	'''
-	distance function
-	'''
-	return math.sqrt(float((p0[0] - p1[0])**2) + float((p0[1] - p1[1])**2))
+    '''
+    distance function
+    '''
+    return math.sqrt(float((p0[0] - p1[0])**2) + float((p0[1] - p1[1])**2))
 
 def debug():
-	print("Hello")
+    print("Hello")
 
 # print(dist((0,0),(5,5)))
